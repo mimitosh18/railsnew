f74a17e27840ef08829b7f908fcb920eb825dcb1
Miraflor G. Buizon <mira.cute25@yahoo.com>
2014-08-15 10:34:20 +0800

    mimi2

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Mimi::Application.config.secret_key_base = '8392c733c00319b7206477c76d264b228eb78b32438358c38cd582d2b70ed5b586a00bd09292234b756bf828af7f03e33c788ccc53d5940b38a63e8ced12a7f3'
